2abc9e42e4128a9009083e0839021f6c8dd4ceed
Marcus Paulo <marcuspaulo.morenomf@gmail.com>
2022-11-29 14:19:37 -0300

    .

--- src/pages/AlterarSenha/AlterarSenha.tsx
+++ src/pages/AlterarSenha/AlterarSenha.tsx
@@ -1,7 +1,16 @@
+import { Box } from "@mui/material";
 import React from "react";
+import { Header } from "../../components/Header/Header";
 
 export const AlterarSenha = () => {
   return (
-    <div>AlterarSenha</div>
+    <>
+      <Header/>
+
+      <Box>
+        
+      </Box>
+    
+    </>
   );
 };
