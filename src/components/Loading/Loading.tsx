1109d0136b73cdbec91efd4cd2682b4214d6ba30
Matheus <matheusrcrv07@gmail.com>
2022-12-04 00:59:18 -0300

    fix: Responsivo loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -4,7 +4,7 @@ export const Loading = () => {
   return (
     <>
       <Box component="main" sx={{ display: "flex", alignItems: "center", justifyContent: "center", width: "100vw", height: "100vh", color: "#fff" }}>
-        <Typography variant="h1">Carregando...</Typography>
+        <Typography sx={{ fontSize: { xs: "50px" } }} variant="h1">Carregando...</Typography>
       </Box>
     </>
   )
